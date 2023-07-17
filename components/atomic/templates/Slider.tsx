f4f61b68c205b7bd9e1f8c33f5e85ab1b157e04c
tsubokura-shuhei <repeat2639@gmail.com>
2023-07-18 01:08:36 +0900

    home fix

--- components/atomic/templates/Slider.tsx
+++ components/atomic/templates/Slider.tsx
@@ -2,35 +2,33 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import style_design from "../../style/design.module.scss";
 import { useSwiperSlide } from "swiper/react";
 
-import { EffectCoverflow, Pagination, Autoplay } from "swiper";
+import { EffectCoverflow, Pagination, Autoplay, Navigation } from "swiper";
 
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
+import { useRef } from "react";
 
 const Slider = () => {
+  const navigationPrevRef = useRef(null);
+  const navigationNextRef = useRef(null);
+
   const swiperSlide = useSwiperSlide();
   return (
     <Swiper
-      // effect={"coverflow"}
+      // effect={"creative"}
       grabCursor={true}
       loop={true}
       centeredSlides={true}
-      slidesPerView={"auto"}
+      slidesPerView={1}
       speed={2000}
       autoplay={{
         delay: 10000,
         disableOnInteraction: false,
       }}
-      coverflowEffect={{
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-      }}
+      navigation={{ prevEl: "#button_prev", nextEl: "#button_next" }}
       pagination={true}
-      modules={[EffectCoverflow, Pagination, Autoplay]}
+      modules={[EffectCoverflow, Pagination, Autoplay, Navigation]}
       className={style_design.image_container}
     >
       <SwiperSlide className={style_design.swiperSlide}>
@@ -70,7 +68,7 @@ const Slider = () => {
       <SwiperSlide className={style_design.swiperSlide}>
         {({ isActive }) => (
           <>
-            <img src="/images/design/image3.jpg" alt="" />
+            <img src="/images/design/image10.jpg" alt="" />
             <div
               className={`${style_design.swiperSlide} ${
                 isActive ? `${style_design.active}` : ""
@@ -87,7 +85,7 @@ const Slider = () => {
       <SwiperSlide className={style_design.swiperSlide}>
         {({ isActive }) => (
           <>
-            <img src="/images/design/image4.jpg" alt="" />
+            <img src="/images/design/image11.jpg" alt="" />
             <div
               className={`${style_design.swiperSlide} ${
                 isActive ? `${style_design.active}` : ""
@@ -101,6 +99,14 @@ const Slider = () => {
           </>
         )}
       </SwiperSlide>
+      <div
+        id="button_prev"
+        className={`${style_design.slider_btn} ${style_design.btn_prev}`}
+      ></div>
+      <div
+        id="button_next"
+        className={`${style_design.slider_btn} ${style_design.btn_next}`}
+      ></div>
     </Swiper>
   );
 };
