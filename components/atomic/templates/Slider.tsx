933f30c445778db7d5861031b9b1fef8783c6858
tsubokura-shuhei <repeat2639@gmail.com>
2023-07-17 16:49:13 +0900

    home fix

--- components/atomic/templates/Slider.tsx
+++ components/atomic/templates/Slider.tsx
@@ -1,5 +1,6 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import style_design from "../../style/design.module.scss";
+import { useSwiperSlide } from "swiper/react";
 
 import { EffectCoverflow, Pagination, Autoplay } from "swiper";
 
@@ -8,6 +9,7 @@ import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 
 const Slider = () => {
+  const swiperSlide = useSwiperSlide();
   return (
     <Swiper
       // effect={"coverflow"}
@@ -31,17 +33,62 @@ const Slider = () => {
       modules={[EffectCoverflow, Pagination, Autoplay]}
       className={style_design.image_container}
     >
-      <SwiperSlide>
-        <img src="/images/design/image1.jpg" alt="" />
+      <SwiperSlide className={style_design.swiperSlide}>
+        {({ isActive }) => (
+          <>
+            <img src="/images/design/image1.jpg" alt="" />
+            <div
+              className={`${style_design.swiperSlide} ${
+                isActive ? `${style_design.active}` : ""
+              }`}
+            >
+              <p>テキスト</p>
+            </div>
+          </>
+        )}
       </SwiperSlide>
-      <SwiperSlide>
-        <img src="/images/design/image2.jpg" alt="" />
+      <SwiperSlide className={style_design.swiperSlide}>
+        {({ isActive }) => (
+          <>
+            <img src="/images/design/image2.jpg" alt="" />
+            <div
+              className={`${style_design.swiperSlide} ${
+                isActive ? `${style_design.active}` : ""
+              }`}
+            >
+              <p>テキスト</p>
+            </div>
+          </>
+        )}
       </SwiperSlide>
-      <SwiperSlide>
-        <img src="/images/design/image3.jpg" alt="" />
+      <SwiperSlide className={style_design.swiperSlide}>
+        {({ isActive }) => (
+          <>
+            <img src="/images/design/image3.jpg" alt="" />
+            <div
+              className={`${style_design.swiperSlide} ${
+                isActive ? `${style_design.active}` : ""
+              }`}
+            >
+              <p>テキスト</p>
+              <p>テキスト</p>
+            </div>
+          </>
+        )}
       </SwiperSlide>
-      <SwiperSlide>
-        <img src="/images/design/image4.jpg" alt="" />
+      <SwiperSlide className={style_design.swiperSlide}>
+        {({ isActive }) => (
+          <>
+            <img src="/images/design/image4.jpg" alt="" />
+            <div
+              className={`${style_design.swiperSlide} ${
+                isActive ? `${style_design.active}` : ""
+              }`}
+            >
+              <p>テキスト</p>
+            </div>
+          </>
+        )}
       </SwiperSlide>
     </Swiper>
   );
