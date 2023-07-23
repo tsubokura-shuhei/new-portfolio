5108b4200ca96c12229c490fc3ec9f3d8c4b2477
tsubokura-shuhei <repeat2639@gmail.com>
2023-07-23 13:02:18 +0900

    business_page create

--- pages/PageBusiness.tsx
+++ pages/PageBusiness.tsx
@@ -7,7 +7,33 @@ import { Layout } from "../components/Layout";
 export default function PageBusiness() {
   return (
     <Layout title="Business">
-      <h1>ビジネスページ</h1>
+      <div className={styles.business}>
+        <h2>
+          BUSINESS<span>ビジネス</span>
+        </h2>
+        <div className={styles.wrapper}>
+          <div className={styles.pr_container}>
+            <h3>
+              PRODUCTION <span>制作</span>
+            </h3>
+            <div className={styles.pr_inner}>
+              <div className={styles.pr_contents}>サイト開発</div>
+              <div className={styles.pr_contents}>LPページ作成</div>
+              <div className={styles.pr_contents}>コミュニティサークル作成</div>
+            </div>
+          </div>
+          <div className={styles.op_container}>
+            <h3>
+              OPERATION <span>運用</span>
+            </h3>
+            <div className={styles.op_inner}>
+              <div className={styles.op_contents}>サイト更新</div>
+              <div className={styles.op_contents}>おすすめ記事更新</div>
+              <div className={styles.op_contents}>情報誌の作成</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
